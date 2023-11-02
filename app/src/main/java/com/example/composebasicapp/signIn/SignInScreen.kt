af9e5023cae2e0a5bfafd2026117271da6b01310
Rashid Saleem <mrashidcit@gmail.com>
2023-11-03 01:11:19 +0500

    Working on Sign In Button

---
+++ app/src/main/java/com/example/composebasicapp/signIn/SignInScreen.kt
@@ -0,0 +1,179 @@
+package com.example.composebasicapp.signIn
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composebasicapp.R
+import com.example.composebasicapp.ui.theme.Black1
+import com.example.composebasicapp.ui.theme.Black2
+import com.example.composebasicapp.ui.theme.ComposeBasicAppTheme
+import com.example.composebasicapp.ui.theme.Gray1
+import com.example.composebasicapp.ui.theme.Gray2
+import com.example.composebasicapp.ui.theme.Gray3
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SignInScreen() {
+
+    val email = remember {
+        mutableStateOf("")
+    }
+    val password = remember {
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Spacer(modifier = Modifier.height(4.dp))
+        Image(
+            modifier = Modifier.size(48.dp),
+            painter = painterResource(id = R.drawable.app_icon),
+            contentDescription = null,
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = "EventHub",
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Medium,
+            color = Black1,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            modifier = Modifier
+                .fillMaxWidth(),
+            text = "Sign in",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Medium,
+            color = Black2,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = email.value,
+            onValueChange = {
+                email.value = it
+            },
+            placeholder = {
+                Text("abc@gmail.com")
+            },
+            leadingIcon = {
+                Icon(
+                    modifier = Modifier.size(22.dp),
+                    painter = painterResource(id = R.drawable.email_icon),
+                    contentDescription = null,
+                )
+            },
+            shape = RoundedCornerShape(18.dp),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                placeholderColor = Gray1,
+                unfocusedLeadingIconColor = Gray2,
+                unfocusedBorderColor = Gray3
+            )
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = password.value,
+            onValueChange = {
+                password.value = it
+            },
+            placeholder = {
+                Text("Your password")
+            },
+            leadingIcon = {
+                Icon(
+                    modifier = Modifier.size(22.dp),
+                    painter = painterResource(id = R.drawable.lock_icon),
+                    contentDescription = null,
+                )
+            },
+            trailingIcon = {
+                Icon(
+                    modifier = Modifier
+                        .size(22.dp)
+                        .clickable { },
+                    painter = painterResource(id = R.drawable.visibility_off_icon),
+                    contentDescription = null,
+                )
+            },
+            shape = RoundedCornerShape(18.dp),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                placeholderColor = Gray1,
+                unfocusedLeadingIconColor = Gray2,
+                unfocusedBorderColor = Gray3
+            )
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Switch(checked = true,
+                onCheckedChange = {
+
+                }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "Remember Me",
+                fontSize = 14.sp,
+                color = Black2,
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = "Forgot Password?",
+                fontSize = 14.sp,
+                color = Black2,
+            )
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        Button(onClick = { /*TODO*/ }) {
+            
+        }
+
+
+    }
+}
+
+@Preview
+@Composable
+fun SignInScreenPreview() {
+    ComposeBasicAppTheme {
+        SignInScreen()
+    }
+}
+
